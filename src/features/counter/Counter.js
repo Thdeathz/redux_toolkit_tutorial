bb0614ddb75593564d90f51901737fa07b33d86e
Thdeathz <buidunga112@gmail.com>
2022-10-15 08:40:33 +0700

    chapter 1

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,35 @@
+import { useMemo, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, reset, incrementByAmount } from "./counterSlice";
+
+function Counter() {
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const addValue = useMemo(() => {
+    return Number(incrementAmount) || 0;
+  }, [incrementAmount]);
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input type="text" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+        <button onClick={() => resetAll()}>Reset All</button>
+      </div>
+    </section>
+  );
+}
+
+export default Counter;
